dd29b725039ae627b94f91572d771383f77e9be4
Phaedra Randolph <phaedra@wannago.com>
2018-01-28 16:37:17 -0800

    Working google scholar response

--- plant_protein_scraper.js
+++ plant_protein_scraper.js
@@ -1,27 +1,47 @@
 var request = require('request');
 var cheerio = require('cheerio');
-var fs = require('fs');
 var scrapeToCSV = require('./writeCSVData');
 
-const BASE_URL = 'https://www.scholar.google.com';
+const SCHOLAR_BASE_URL = 'http://scholar.google.com/';
 
-request("https://www.reddit.com", function(error, response, body) {
-  if (error) {
-    console.log("Error: " + error);
+var makeQueryStringFromTerms = (terms) => {
+  if (!terms || terms.length < 1) {
+    console.log("Error: missing search terms");
+    return;
   }
-  console.log("Status code: " + response.statusCode);
 
-  var $ = cheerio.load(body);
-  var data = [];
-  $('div#siteTable > div.link').each(function( index ) {
-    var title = $(this).find('p.title > a.title').text().trim();
-    var score = $(this).find('div.score.unvoted').text().trim();
-    var user = $(this).find('a.author').text().trim();
-    console.log("Title: " + title);
-    console.log("Score: " + score);
-    console.log("User: " + user);
-    data.push([title, score, user]);
-    fs.appendFileSync('reddit.txt', title + '\n' + score + '\n' + user + '\n');
+  var query = '';
+  const end = terms.length - 1;
+  terms.forEach((term, idx) => {
+    query += term;
+    if (idx != end) {
+      query += '+';
+    }
   });
-  scrapeToCSV.write('test.csv', ['title', 'score', 'user'], data);
-});
+  return query;
+};
+
+var getURLFromSearchTerms = (terms) => {
+  return `${SCHOLAR_BASE_URL}scholar?hl=en&as_sdt=0%2C5&q=${makeQueryStringFromTerms(terms)}`;
+};
+
+var searchAndScrape = (terms, foodCateogry, dataCategory) => {
+  request(getURLFromSearchTerms(terms), (error, response, body) => {
+    if (error) {
+      console.log("Error: " + error);
+    }
+    console.log("Status code: " + response.statusCode);
+
+    var $ = cheerio.load(body);
+    var data = [];
+    $('div#siteTable > div.link').each(index => {
+      var title = $(this).find('p.title > a.title').text().trim();
+      var score = $(this).find('div.score.unvoted').text().trim();
+      var user = $(this).find('a.author').text().trim();
+      data.push([title, score, user]);
+    });
+    scrapeToCSV.write('test.csv', ['title', 'score', 'user'], data);
+  });
+};
+
+searchAndScrape(['bean', 'protein', 'properties'], 'legumes', 'protein');
